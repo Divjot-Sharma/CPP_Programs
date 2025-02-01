b0ce2eb3ff744195ad622f7b662fb11ea534bbf3
Divjot Sharma <162088342+Divjot-Sharma@users.noreply.github.com>
2025-01-31 21:36:49 -0800

    Update main.cpp

--- C-Programs/Number_Guessing_Game/main.cpp
+++ C-Programs/Number_Guessing_Game/main.cpp
@@ -1,1 +1,22 @@
-
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(){
+    srand(time(NULL));
+    int num = rand() % 100 + 1;
+    int guess=-1;
+    int tries=0;
+    printf("Hi I am Number Guesser Game .\n");
+    while(guess!=num){ 
+    printf("Enter Number :");
+    scanf("%d",&guess);
+    if (guess<num){
+        printf("Enter Higher Number Please!\n");
+    }else if (guess>num){
+        printf("Enter Lower Number Please!\n");
+    }
+        tries++;
+    }
+    printf("You Won.\nNumber of tries: %d",tries);
+    return 0 ; 
+}
